db1e259f83ed32c40ab75aa47c1a51b089f3a3e8
Vishaka Nirmal <vnirmal@uw.edu>
2020-01-17 08:52:23 -0800

    initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+
+    <header> 
+      Chats
+     <img src = "https://i.ya-webdesign.com/images/speaking-clipart-quotation-9.png"
+        alt = "logo"
+        className="logo" />
+    
+    
+    </header>
+    <TextInput onSend={t=> console.log(t)} />
+    
+  </main>
 }
+
+function TextInput(props) {
+  const [text, setText] = useState('')
+
+  return <div className="text-input-wrap">
+    <input value={text}
+      placeholder="Message"
+      onChange={e=> setText(e.target.value)}
+      className="text-input"
+    
+    />
+    <button onClick={()=> {
+      props.onSend(text)
+      setText('')
+    }}
+    className="button">
+    <img src = "https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png"
+        alt = "icon"
+        className="icon" />
+    </button>
+    
+  </div>
+
+}
+
 
 export default App;
